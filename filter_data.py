cfeef11485cab62a7aa8146b5c1ce09de28124a4
Saanvi Tyagi <saanvi7168@outlook.com>
2026-03-23 23:06:00 -0400

    Update filter_data.py

--- filter_data.py
+++ filter_data.py
@@ -1,1 +1,11 @@
-"hi"
+"""filter data"""
+
+import csv
+
+
+def load_reddit_psychometric(mbti_file: str) -> None:
+    with open(mbti_file, newline='') as file:
+        csv_reader = csv.reader(file)
+
+
+
